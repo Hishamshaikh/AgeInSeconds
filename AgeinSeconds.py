918f5e80b3aaed764ce84ea8c5b143fa28cca596
Hisham Shaikh <hisshaam.shaikh@gmail.com>
2020-04-10 14:51:37 +0530

    Included months also in this program.

--- AgeinSeconds.py
+++ AgeinSeconds.py
@@ -1,6 +1,12 @@
 def age_in_seconds():
-    user_age = input("Enter your age: ")
-    age_seconds = int(user_age) * 365 * 24 * 60 * 60
-    print("You have lived for {} seconds.".format(age_seconds))
+    user_age, user_month = input("Enter your age in years & month: ").split()
+    print("Age entered: ", user_age)
+    print("Months entered: ", user_month)
+    age_seconds = (int(user_age) * 365 * 24 * 60 * 60)
+    month_seconds = (int(user_month) * 30 * 24 * 60 * 60)
+    total_seconds = age_seconds + month_seconds
+    print(age_seconds)
+    print(month_seconds)
+    print("You have lived for {} seconds.".format(total_seconds))
 
 age_in_seconds()
